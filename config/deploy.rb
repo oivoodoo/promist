11585c8df0322e71353f71f9f9cd28b83d3e9f18
Alexander Korsak <alex.korsak@gmail.com>
2012-04-08 23:14:59 +0300

    Change deployment script to use production

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,13 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "promist.ru"
+set :repository,  "git@github.com:oivoodoo/promist.git"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, "rails"
+set :scm, :git
+set :branch, "master"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "176.58.96.250"
+role :app, "promist.ru"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+default_run_options[:pty] = true
+set :deploy_to, "~/promist.ru/"
