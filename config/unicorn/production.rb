2bead1793797fef75fab4d00d6b27f5385e694d9
Alexander Korsak <alex.korsak@gmail.com>
2012-04-09 00:43:17 +0300

    Change configuration of production

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,82 +1,23 @@
-rails_env = 'production'
+worker_processes 2
+working_directory "/home/rails/promist.ru/current/"
 
-# 16 workers and 1 master
-worker_processes (rails_env == 'production' ? 16 : 4)
-
-# Load rails+github.git into the master before forking workers
-# for super-fast worker spawn times
 preload_app true
 
-# Restart any workers that haven't responded in 30 seconds
 timeout 30
 
-# Listen on a Unix data socket
-listen '/home/rails/promist.ru/shared/tmp/sockets/unicorn.sock', :backlog => 2048
+listen "/home/rails/promist.ru/shared/tmp/sockets/unicorn.sock", :backlog => 64
 
-##
-# REE
+pid "/home/rails/promist.ru/shared/tmp/pids/unicorn.pid"
 
-# http://www.rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-if GC.respond_to?(:copy_on_write_friendly=)
-  GC.copy_on_write_friendly = true
-end
-
+stderr_path "/home/rails/promist.ru/shared/log/unicorn.stderr.log"
+stdout_path "/home/rails/promist.ru/shared/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
-
-  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
+    defined?(ActiveRecord::Base) and
+        ActiveRecord::Base.connection.disconnect!
 end
 
-
 after_fork do |server, worker|
-  ##
-  # Unicorn master loads the app then forks off workers - because of the way
-  # Unix forking works, we need to make sure we aren't using any of the parent's
-  # sockets, e.g. db connection
-
-  ActiveRecord::Base.establish_connection
-  CHIMNEY.client.connect_to_server
-  # Redis and Memcached would go here but their connections are established
-  # on demand, so the master never opens a socket
-
-
-  ##
-  # Unicorn master is started as root, which is fine, but let's
-  # drop the workers to git:git
-
-  begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'git', 'git'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
-  rescue => e
-    if RAILS_ENV == 'development'
-      STDERR.puts "couldn't change user, oh well"
-    else
-      raise e
-    end
-  end
+    defined?(ActiveRecord::Base) and
+        ActiveRecord::Base.establish_connection
 end
