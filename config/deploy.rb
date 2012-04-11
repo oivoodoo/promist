4e6654fdcd6be628934897754fd70905e9b362c8
Alexander Korsak <alex.korsak@gmail.com>
2012-04-11 19:48:09 +0300

    Do not try to start unicorn after deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,9 @@
-require 'bundler/capistrano'
-
 set :application, "promist.ru"
 set :repository,  "git@github.com:oivoodoo/promist.git"
 set :user, "rails"
 set :scm, :git
 set :branch, "master"
-set :unicorn_bin, 'unicorn_rails'
 role :web, "176.58.96.250"
-role :app, "176.58.96.250"
 
 default_run_options[:pty] = true
 set :deploy_to, "~/promist.ru/"
