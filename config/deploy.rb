68e50cb65a7fcbfdc8044bc858b3b75ab1cc67e7
Alexander Korsak <alex.korsak@gmail.com>
2012-04-11 18:19:13 +0300

    Change deployment config for unicorn_rails running

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,9 @@ set :repository,  "git@github.com:oivoodoo/promist.git"
 set :user, "rails"
 set :scm, :git
 set :branch, "master"
+set :unicorn_bin, 'unicorn_rails'
 role :web, "176.58.96.250"
+role :app, "176.58.96.250"
 
 default_run_options[:pty] = true
 set :deploy_to, "~/promist.ru/"
