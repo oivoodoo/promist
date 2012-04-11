dc370ae741575b4e08d29478bcfc15433c2fdefd
Alexander Korsak <alex.korsak@gmail.com>
2012-04-11 19:39:30 +0300

    Add database symlink creation to the project

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,5 +10,11 @@ role :app, "176.58.96.250"
 default_run_options[:pty] = true
 set :deploy_to, "~/promist.ru/"
 
+after 'deploy' do
+  run <<-CMD
+    ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml
+  CMD
+end
+
 require 'capistrano-unicorn'
 
